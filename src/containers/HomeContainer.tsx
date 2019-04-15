7c41fe62ddc18ba81b836cf22103fd460e144889
khelmy <khelmy@andrew.cmu.edu>
2019-04-15 11:24:25 -0400

    Update home container

--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -51,7 +51,7 @@ const Home: React.FunctionComponent = () => {
             </Col>
           </Row>
           <Row className="pt-5">
-            <Col xs={6} sm={6} md={6} lg={4} className="text-center ml-auto">
+            <Col xs={6} sm={6} md={6} lg={4} className="ml-auto mr-auto text-center">
               <MdSecurity size={50} className="text-zdex" />
               <div className="text-secondary text-center pt-3">
                 <b>
@@ -63,20 +63,6 @@ const Home: React.FunctionComponent = () => {
                   <small>
                     ZDEX is free and open-source. Keystore encryption and decryption are handled on
                     your computer only.
-                  </small>
-                </p>
-              </div>
-            </Col>
-            <Col xs={6} sm={6} md={6} lg={4} className="text-center mr-auto">
-              <MdBeenhere size={50} className="text-zdex" />
-              <div className="text-secondary text-center pt-3">
-                <b>We recommend downloading and securely storing a keystore json file.</b>
-                <br />
-                <p className="pt-1">
-                  <small>
-                    keystore json file contains your encrypted private key, which can only be
-                    decrypted with a password you choose. If you lose your private key or the
-                    password to your keystore json, it cannot be recovered by anyone.
                   </small>
                 </p>
               </div>
